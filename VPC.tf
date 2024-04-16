2528553be22ffce5b1f9f95a550959c1adb65364
chrisgonz65 <g.chris65@yahoo.com>
2024-04-16 08:23:09 -0700

    updated vpc and ec2 build

--- VPC.tf
+++ VPC.tf
@@ -44,18 +44,6 @@ tags = {
 }
 }
 
-#EC2 build with linux AMI in public subnet
-resource "aws_instance" "server2"{
-ami = "ami-07413789aae50b0e0"
-#key_name = ""
-instance_type = "t2.micro"
-subnet_id = aws_subnet.public1.id
-
-tags = {
-    Name = "AWS_VPC1"
-}
-}
-
 #internet gateway for public1 subnet
 resource "aws_internet_gateway" "IG-01"{
     vpc_id = aws_vpc.VPC1.id
--- apache_server.tf
+++ apache_server.tf
@@ -2,7 +2,14 @@ provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws_instance" "apache_server" {
-    ami = "ami-07413789aae50b0e0"
-    instance_type = "t2.micro"
+#EC2 build with linux AMI in public subnet
+resource "aws_instance" "server2"{
+ami = "ami-07413789aae50b0e0"
+#key_name = ""
+instance_type = "t2.micro"
+subnet_id = aws_subnet.public1.id
+
+tags = {
+    Name = "AWS_VPC1"
+}
 }
