aeb71e28c7896d9417ed289d3a4b969d60d9c755
chrisgonz65 <g.chris65@yahoo.com>
2024-04-22 09:22:35 -0700

    updating variable and main

--- Project2_modules/main.tf
+++ Project2_modules/main.tf
@@ -64,60 +64,60 @@ resource "aws_route_table" "public_route_table" {
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
+  subnet_id           = aws_subnet.public_subnet_az1.id
+  route_table_id      = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
+  subnet_id           = aws_subnet.public_subnet_az2.id
+  route_table_id      = aws_route_table.public_route_table.id
 }
 
 # create private app subnet az1
 resource "aws_subnet" "private_app_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.public_subnet_az1_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private app subnet az1"
   }
 }
 
 # create private app subnet az2
 resource "aws_subnet" "private_app_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_app_subnet_az2_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private app subnet az2"
   }
 }
 
 # create private data subnet az1
 resource "aws_subnet" "private_data_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vps.vpc.id
+  cidr_block               = var.private_data_subnet_az1_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private data subnet az1"
   }
 }
 
 # create private data subnet az2
 resource "aws_subnet" "private_data_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_data_subnet_az2_cidr
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = 
+    Name    = "private data subnet az2"
   }
 }
--- Project2_modules/outputs.tf
+++ Project2_modules/outputs.tf
@@ -1,3 +1,15 @@
+output "region" {
+  value = var.region
+}
+
+output "project_name" {
+  value = var.project_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
 /*  allow you to export structured data about your 
 resources. Outputs are also necessary to share data 
 from a child module to your root module. to get information 
